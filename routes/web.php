523cf32b11bdf29679b7d39b40e0622d522012bd
Saba <saba.lapanashvili15@gmail.com>
2022-11-02 11:38:29 +0400

    .

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', [PostController::class,"index"])->name("home");
 
 Route::get("posts/{post:slug}",[PostController::class,"show"]);
-
-Route::get("categories/{category:slug}",function (Category $category) {
-    return view("posts", [
-        "posts" => $category->posts,
-        "currentCategory" => $category,
-        "categories" => Category::all()
-    ]);
-})->name("category");
-
-Route::get("authors/{author:username}",function (User $author) {
-    return view("posts", [
-        "posts" => $author->posts,
-        "categories" => Category::all()
-    ]);
-});

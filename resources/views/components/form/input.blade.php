6103454b200e3eb3852fc490d82470e65dac8395
Saba <saba.lapanashvili15@gmail.com>
2022-11-04 16:44:57 +0400

    .

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,13 @@
+@props(["name"])
+<x-form.field>
+    <x-form.label name="{{$name}}" />
+    <input class="border border-gray-400 p-2 w-full rounded"
+     type="{{$name}}"
+     name="{{$name}}"
+     id="{{$name}}"
+     value="{{old($name)}}"
+     required
+     {{$attributes}}
+     >
+     <x-form.error name="{{$name}}" />
+</x-form.field>
